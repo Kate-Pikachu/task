0efdafa870468eff5556717b51896f24958f33a8
Kate-Pikachu <pikulina.ev@phystech.edu>
2021-02-10 13:11:41 +0300

    changed line

--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     std::cout << "Hello Everybody xexexexe i am your father Luke!\n";
-    std::cout << "LoL" << std::endl;
+    std::cout << "xaxaxa" << std::endl;
     std::cout << "meow" << std::endl;
     std::cout << "gagaga" << std::endl;
     std::cout << "help" << std::endl;
